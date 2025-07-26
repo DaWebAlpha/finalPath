b0a542265633b2e865b91b2510f7173361323d0f
DaWebAlpha <tribillio@gmail.com>
2025-07-26 16:40:06 -0700

    added backed

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const MONGO_URI = process.env.MONGO_URI
+mongoose.connect(MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", ()=>{
+    console.log("Database is connected");
+})
+
+mongoose.connection.on("error", (err)=>{
+    console.log(`Database could not connect ${err}`);
+})
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("Database has been disconnected");
+})
